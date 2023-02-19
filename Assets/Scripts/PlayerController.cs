0bf08d94af1a6fe31829f0e4f46e6e89aeec3a38
ishigakimasahiro <87878259+ishigakimasahiro@users.noreply.github.com>
2023-02-19 23:26:53 +0900

    commit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,74 +7,73 @@ public class PlayerController : MonoBehaviour
     bool isMoving;
     [SerializeField] Animator animator;
 
-    [SerializeField] LayerMask longGrassLayer;
+    [SerializeField] LayerMask SolidObjectsLayer;
+
+    private Rigidbody2D rb;
+    private float x_val;
+    private float speed;
+    public float inputSpeed;
 
     void Start()
     {
-        transform.position = new Vector3(0.5f, 0.5f, 0);
+        rb = GetComponent<Rigidbody2D>();
     }
-
     void Update()
     {
-        if (isMoving == false)
+        x_val = Input.GetAxis("Horizontal");
+    }
+    void FixedUpdate()
+    {
+        isMoving = false;
+
+        //待機
+        if (x_val == 0)
         {
-            float x = Input.GetAxisRaw("Horizontal");
-            float y = Input.GetAxisRaw("Vertical");
+            speed = 0;
+        }
+        //右に移動
+        else if (x_val > 0)
+        {
+            speed = inputSpeed;
+            MoveSet();
+        }
+        //左に移動
+        else if (x_val < 0)
+        {
+            speed = inputSpeed * -1;
+            MoveSet();
+        }
 
-            if(x!=0)
+        // キャラクターを移動 Vextor2(x軸スピード、y軸スピード(元のまま))
+        rb.velocity = new Vector2(speed, rb.velocity.y);
+
+        CheckForEncounters();
+    }
+
+    void MoveSet()
+    {
+        isMoving = true;
+
+        if (Input.GetAxisRaw("Horizontal") != 0)
+        {
+            if (Input.GetAxisRaw("Horizontal") > 0)
             {
-                y = 0;
-            }
-
-            StartCoroutine(Move(new Vector2(x, y)));
-
-
-            if (Input.GetAxisRaw("Horizontal") != 0)
-            {
-                if (Input.GetAxisRaw("Horizontal") > 0)
-                {
-                    animator.SetFloat("InputX", 1f);
-                    animator.SetFloat("InputY", 0);
-                }
-                else
-                {
-                    animator.SetFloat("InputX", -1f);
-                    animator.SetFloat("InputY", 0);
-                }
-            }
-            else if (Input.GetAxisRaw("Vertical") > 0)
-            {
-                animator.SetFloat("InputX", 0);
-                animator.SetFloat("InputY", 1);
+                animator.SetFloat("InputX", 1f);
+                animator.SetFloat("InputY", 0);
             }
             else
             {
-                animator.SetFloat("InputX", 0);
-                animator.SetFloat("InputY", -1);
+                animator.SetFloat("InputX", -1f);
+                animator.SetFloat("InputY", 0);
             }
-
-            animator.SetBool("isMoving",isMoving);
-
         }
-    }
 
-    IEnumerator Move(Vector3 direction)
-    {
-        isMoving = true;
-        Vector3 targetPos = transform.position + direction;
-        while((targetPos - transform.position).sqrMagnitude>Mathf.Epsilon)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, targetPos, 5f * Time.deltaTime);
-            yield return null;
-        }
-        transform.position = targetPos;
-        isMoving = false;
-        CheckForEncounters();
+        animator.SetBool("isMoving", isMoving);
     }
 
     void CheckForEncounters()
     {
-        if(Physics2D.OverlapCircle(transform.position,0.2f,longGrassLayer))
+        if(Physics2D.OverlapCircle(transform.position,0.2f, SolidObjectsLayer))
         {
             if(Random.Range(0,100)<6)
             {
